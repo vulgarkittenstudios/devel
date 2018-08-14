9ba96a3507bf39df23be855dbdafa3b4e5263a70
theallmightyjohn <john.manning89@gmail.com>
2018-08-13 20:16:38 -0400

    updated git ignore to i clude rules for composer

---
+++ fix-sync.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+var file = process.env.HOME+'/devel/node_modules/resolve/lib/node-modules-paths.js';
+var str = 'process.env.NODE_PATH';
+fs.readFile(file, 'utf8', function (err,data) {
+
+  if (err) {
+    return console.log(err);
+  }
+ 
+ var result = 
+data.replace(/\'node_modules\'/g, str);
+
+  fs.writeFile(file, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
